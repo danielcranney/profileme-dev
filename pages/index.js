2b430dcc57cac090ef82db84056e6438e3bbe8f8
djcranney <danieljamescranney@gmail.com>
2022-03-07 19:43:24 +0000

    Error Fix

--- pages/index.js
+++ pages/index.js
@@ -501,7 +501,7 @@ export default function Home() {
           }}
         >
           <svg
-            class={`w-6 h-6 transition-all duration-150 ease-in-out ${
+            className={`w-6 h-6 transition-all duration-150 ease-in-out ${
               sidebarOpen ? "text-white" : "text-brand"
             }`}
             fill="none"
@@ -510,9 +510,9 @@ export default function Home() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 10h16M4 14h16M4 18h16"
             ></path>
           </svg>

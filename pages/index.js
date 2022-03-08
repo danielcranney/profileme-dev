b9a5c06f32da5d93ff370b7423bd198d7e8fc5fa
djcranney <danieljamescranney@gmail.com>
2022-03-08 17:24:41 +0000

    Logo Removal

--- pages/index.js
+++ pages/index.js
@@ -500,10 +500,7 @@ export default function Home() {
           property="twitter:description"
           content="Create an awesome GitHub profile in minutes"
         />
-        <meta
-          property="twitter:creator"
-          content="Create an awesome GitHub profile in minutes"
-        />
+        <meta property="twitter:creator" content="@danielcranney" />
       </Head>
       <header className="fixed z-40 flex items-center w-full h-16 px-6 border-b md:relative bg-dark-800 border-dark-600">
         <button
@@ -529,20 +526,7 @@ export default function Home() {
             ></path>
           </svg>
         </button>
-        <svg
-          className="w-6 h-6 mr-2 text-brand"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-          ></path>
-        </svg>
+
         <h1 className="mb-0 text-base md:text-xl">
           ProfileMe<span className="text-brand">.dev</span>
         </h1>

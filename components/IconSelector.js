8b81697c37996f82028b2a252398e13218d99b81
djcranney <danieljamescranney@gmail.com>
2022-04-07 19:33:40 +0100

    Z-Index Fixed

--- components/IconSelector.js
+++ components/IconSelector.js
@@ -31,7 +31,7 @@ const IconSelector = ({ handleIconToggle, title, iconType }) => {
                 </p>
               </div>
               {state.skills[iconType].includes(icon) ? (
-                <div className="absolute top-0 left-0 w-4 h-4 p-0 overflow-hidden text-xs bg-white border-0 rounded-lg z-20">
+                <div className="absolute top-0 left-0 w-4 h-4 p-0 overflow-hidden text-xs bg-white border-0 rounded-lg z-10">
                   <ExtraSmallTick />
                 </div>
               ) : null}

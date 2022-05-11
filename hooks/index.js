0daee0d35ca9e439de3f8648a1fb1762761a5ad2
Adam King <6437643+AdamJAKing@users.noreply.github.com>
2022-05-12 00:41:03 +0100

    Added the ability to provide before and after middleware to the useReducer hook Added the ability for the state to be saved to localstorage

---
+++ hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useReducerWithMiddleware } from './useReducerWithMiddleware';
---
+++ hooks/useReducerWithMiddleware.js
@@ -0,0 +1,36 @@
+import React, { useReducer, useRef } from 'react';
+
+const useReducerWithMiddleware = (
+    reducer,
+    initialState,
+    beforeMiddlewareFns,
+    afterMiddlewareFns
+) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const aRef = useRef();
+
+    const dispatchWithMiddleware = (action) => {
+        beforeMiddlewareFns.forEach((beforeMiddlewareFn) =>
+            beforeMiddlewareFn(action, state)
+        );
+
+        aRef.current = action;
+
+        dispatch(action);
+    };
+
+    React.useEffect(() => {
+        if (!aRef.current) return;
+
+        afterMiddlewareFns.forEach((afterwareMiddlewareFn) =>
+            afterwareMiddlewareFn(aRef.current, state)
+        );
+
+        aRef.current = null;
+    }, [afterMiddlewareFns, state]);
+
+    return [state, dispatchWithMiddleware];
+};
+
+export default useReducerWithMiddleware;

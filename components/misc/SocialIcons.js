e7f377ebd398ca402a64800d92e494b2a6ab30c2
Dharmik48 <hingudharmik4@gmail.com>
2022-06-25 16:49:42 +0530

    fix broken link of github and twitter in footer

--- components/misc/SocialIcons.js
+++ components/misc/SocialIcons.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import TwitterIcon from './TwitterIcon';
+import GitHubIcon from './GitHubIcon';
 
 export const SocialIcons = () => {
-  return (
-    <div className="flex items-center gap-x-1.5">
-      <i className="devicon-github-original text-2xl text-dark-500 hover:text-dark-700 dark:text-light-400 dark:hover:text-white cursor-pointer transition-all duration-150 ease-in-out"></i>
-
-      <i className="devicon-twitter-original text-xl text-dark-500 hover:text-dark-700 dark:text-light-400 dark:hover:text-white cursor-pointer transition-all duration-150 ease-in-out"></i>
-    </div>
-  );
+	return (
+		<div className='flex items-center gap-x-1.5'>
+			<GitHubIcon />
+			<TwitterIcon />
+		</div>
+	);
 };

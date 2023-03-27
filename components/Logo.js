fba2c3b17813c5745fe2581345225c6eab1e55d8
djcranney <danieljamescranney@gmail.com>
2023-03-27 08:26:44 +0100

    :+1: Package Updates / Sponsor Button Added

--- components/Logo.js
+++ components/Logo.js
@@ -6,21 +6,20 @@ import { motion } from "framer-motion";
 const Logo = () => {
   const { state, dispatch } = useContext(StateContext);
   return (
-    <Link href={"/"}>
-      <motion.a
-        className={`font-bold tracking-tight mb-0 text-lg sm:text-xl transition-all duration-150 ease-in-out no-underline text-dark-900 dark:text-white`}
+    <Link
+      href={"/"}
+      className={`font-bold tracking-tight mb-0 text-lg sm:text-xl transition-all duration-150 ease-in-out no-underline text-dark-900 dark:text-white`}
+    >
+      ProfileMe
+      <span
+        className={`transition-all duration-150 ease-in-out ${
+          state.sidebarOpen
+            ? "text-brand-alt dark:text-brand"
+            : "text-brand-alt dark:text-brand"
+        }`}
       >
-        ProfileMe
-        <span
-          className={`transition-all duration-150 ease-in-out ${
-            state.sidebarOpen
-              ? "text-brand-alt dark:text-brand"
-              : "text-brand-alt dark:text-brand"
-          }`}
-        >
-          .dev
-        </span>
-      </motion.a>
+        .dev
+      </span>
     </Link>
   );
 };

63f08ac6eaa7cdca83b480f28c40b87a4999bcd6
mehmet.kirkoca <mehmet_kirkoca@hotmail.com>
2023-08-06 08:17:35 +0300

    youtube linkPrefix fixed

--- pages/_app.js
+++ pages/_app.js
@@ -922,7 +922,7 @@ const initialState = {
     },
     youtube: {
       path: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/youtube.svg",
-      linkPrefix: "https://www.youtube.com/c/",
+      linkPrefix: "https://www.youtube.com/",
       linkSuffix: "",
     },
     discord: {

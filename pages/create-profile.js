2088b937974e3370e9fcd59d51575c519893a98a
Netervati <netervati@gmail.com>
2023-11-05 19:01:08 +0800

    Remove conditions for withSupport

--- pages/create-profile.js
+++ pages/create-profile.js
@@ -72,16 +72,13 @@ export default function CreateProfile() {
   const [socialsShowing, setSocialsShowing] = useState(false);
   const [badgesShowing, setBadgesShowing] = useState(false);
   const [copySuccess, setCopySuccess] = useState("Copy");
-  const withSupport =
-    state && state.support && Array.isArray(state.support)
-      ? Object.values(state.support).some(
-          (value) =>
-            value &&
-            value.linkSuffix !== undefined &&
-            value.linkSuffix !== null &&
-            value.linkSuffix !== ""
-        )
-      : false;
+  const withSupport = Object.values(state.support).some(
+    (value) =>
+      value &&
+      value.linkSuffix !== undefined &&
+      value.linkSuffix !== null &&
+      value.linkSuffix !== ""
+  )
 
   function build_markdown_skill(category) {
     return (

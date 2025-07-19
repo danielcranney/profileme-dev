2df20b9521d3e3c49c0ee0904d7cb26e840b7cd6
Daniel Cranney <hello@danielcranney.com>
2025-07-19 18:00:42 +0100

    Chore: Change Name of Local State Key
    Local state key updated. Users will previously-saved local state would see error as object has since changed shape.

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import storeStateMiddleware from "../middleware/storeStateMiddleware";
 
 export const StateContext = createContext(null);
 
-export const STORED_STATE_KEY = "profileMeLocalStateMar24";
+export const STORED_STATE_KEY = "ProfileMe_LocalState";
 
 const PROJECT_URL =
   process.env.NODE_ENV === "production"

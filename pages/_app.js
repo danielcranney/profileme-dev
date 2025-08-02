73d18fc64f6b755e5402a7371e10035ff07aa30a
Daniel Cranney <hello@danielcranney.com>
2025-08-02 11:18:15 +0100

    Fix: Add Missing GitLab label

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import React, { createContext, useEffect } from 'react';
-import '../styles/globals.css';
-import Script from 'next/script';
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
-import { ThemeProvider } from 'next-themes';
-import { useReducerWithMiddleware } from '../hooks';
-import storeStateMiddleware from '../middleware/storeStateMiddleware';
+import React, { createContext, useEffect } from "react";
+import "../styles/globals.css";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import { ThemeProvider } from "next-themes";
+import { useReducerWithMiddleware } from "../hooks";
+import storeStateMiddleware from "../middleware/storeStateMiddleware";
 
 export const StateContext = createContext(null);
 
@@ -15,7 +15,7 @@ const PROJECT_URL =
   process.env.NODE_ENV === "production"
     ? "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public"
     : "http://localhost:3000";
- 
+
 export const SKILL_CATEGORIES = [
   { name: "core", label: "Core" },
   { name: "scripting", label: "Scripting" },
@@ -26,11 +26,11 @@ export const SKILL_CATEGORIES = [
   { name: "web3", label: "Web3" },
   { name: "cloud", label: "Cloud" },
   { name: "cms", label: "CMS" },
-  { name: "embedded", label: "Embedded"},
-  { name: "operatingSystem", label: "Operating Systems"},
+  { name: "embedded", label: "Embedded" },
+  { name: "operatingSystem", label: "Operating Systems" },
   { name: "other", label: "Other" },
 ];
- 
+
 export const iconData = {
   core: [
     {
@@ -318,7 +318,7 @@ export const iconData = {
       name: "Servomon",
       path: `${PROJECT_URL}/icons/skills/servomon-colored.svg`,
       iTag: "servomon",
-      link: "https://servemon.netlify.app/"
+      link: "https://servemon.netlify.app/",
     },
     {
       name: "Vite",
@@ -806,206 +806,205 @@ export const iconData = {
   ],
 };
 
-
 export const ACTIONS = {
-  HYDRATE_STORED_STATE: 'hydrate-stored-state',
-  ADD_INTRODUCTION: 'add-introduction',
-  SELECT_RENDER_MODE: 'select-render-mode',
-  ADD_SKILL: 'add-skill',
-  REMOVE_SKILL: 'remove-skill',
-  ADD_SOCIAL_PROFILE: 'add-social-profile',
-  ADD_ALTERNATIVE_SOCIAL_PROFILE: 'add-alternative-social-profile',
-  TOGGLE_BADGE: 'toggle-badge',
-  TOGGLE_GITHUB_STATS: 'toggle-github-stats',
-  STYLE_BADGES: 'style-badges',
-  TOGGLE_STYLE_COLOR: 'toggle-style-color',
-  ADD_REPO: 'add-repo',
-  DELETE_REPO: 'delete-repo',
-  ADD_SUPPORT: 'add-support',
-  TOGGLE_COPY_MODAL: 'toggle-copy-modal',
-  TOGGLE_ELEMENT: 'toggle-element',
-  CLOSE_ELEMENT: 'close-element',
+  HYDRATE_STORED_STATE: "hydrate-stored-state",
+  ADD_INTRODUCTION: "add-introduction",
+  SELECT_RENDER_MODE: "select-render-mode",
+  ADD_SKILL: "add-skill",
+  REMOVE_SKILL: "remove-skill",
+  ADD_SOCIAL_PROFILE: "add-social-profile",
+  ADD_ALTERNATIVE_SOCIAL_PROFILE: "add-alternative-social-profile",
+  TOGGLE_BADGE: "toggle-badge",
+  TOGGLE_GITHUB_STATS: "toggle-github-stats",
+  STYLE_BADGES: "style-badges",
+  TOGGLE_STYLE_COLOR: "toggle-style-color",
+  ADD_REPO: "add-repo",
+  DELETE_REPO: "delete-repo",
+  ADD_SUPPORT: "add-support",
+  TOGGLE_COPY_MODAL: "toggle-copy-modal",
+  TOGGLE_ELEMENT: "toggle-element",
+  CLOSE_ELEMENT: "close-element",
 };
 
 // Color Store
 export const colorStore = {
   lightColors: [
     {
-      colorName: 'white',
-      bgColor: 'bg-[#ffffff]',
-      hex: 'ffffff',
+      colorName: "white",
+      bgColor: "bg-[#ffffff]",
+      hex: "ffffff",
     },
     {
-      colorName: 'black',
-      bgColor: 'bg-[#000000]',
-      hex: '000000',
+      colorName: "black",
+      bgColor: "bg-[#000000]",
+      hex: "000000",
     },
     {
-      colorName: 'slate-dark',
-      bgColor: 'bg-[#0f172a]',
-      hex: '0f172a',
+      colorName: "slate-dark",
+      bgColor: "bg-[#0f172a]",
+      hex: "0f172a",
     },
     {
-      colorName: 'red',
-      bgColor: 'bg-[#ef4444]',
-      hex: 'ef4444',
+      colorName: "red",
+      bgColor: "bg-[#ef4444]",
+      hex: "ef4444",
     },
     {
-      colorName: 'orange',
-      bgColor: 'bg-[#f97316]',
-      hex: 'f97316',
+      colorName: "orange",
+      bgColor: "bg-[#f97316]",
+      hex: "f97316",
     },
     {
-      colorName: 'yellow',
-      bgColor: 'bg-[#facc15]',
-      hex: 'facc15',
+      colorName: "yellow",
+      bgColor: "bg-[#facc15]",
+      hex: "facc15",
     },
     {
-      colorName: 'lime',
-      bgColor: 'bg-[#84cc16]',
-      hex: '84cc16',
+      colorName: "lime",
+      bgColor: "bg-[#84cc16]",
+      hex: "84cc16",
     },
     {
-      colorName: 'green',
-      bgColor: 'bg-[#22c55e]',
-      hex: '22c55e',
+      colorName: "green",
+      bgColor: "bg-[#22c55e]",
+      hex: "22c55e",
     },
     {
-      colorName: 'emerald',
-      bgColor: 'bg-[#10b981]',
-      hex: '10b981',
+      colorName: "emerald",
+      bgColor: "bg-[#10b981]",
+      hex: "10b981",
     },
     {
-      colorName: 'teal',
-      bgColor: 'bg-[#14b8a6]',
-      hex: '14b8a6',
+      colorName: "teal",
+      bgColor: "bg-[#14b8a6]",
+      hex: "14b8a6",
     },
     {
-      colorName: 'cyan',
-      bgColor: 'bg-[#0891b2]',
-      hex: '0891b2',
+      colorName: "cyan",
+      bgColor: "bg-[#0891b2]",
+      hex: "0891b2",
     },
     {
-      colorName: 'blue',
-      bgColor: 'bg-[#3382ed]',
-      hex: '3382ed',
+      colorName: "blue",
+      bgColor: "bg-[#3382ed]",
+      hex: "3382ed",
     },
     {
-      colorName: 'indigo',
-      bgColor: 'bg-[#6366f1]',
-      hex: '6366f1',
+      colorName: "indigo",
+      bgColor: "bg-[#6366f1]",
+      hex: "6366f1",
     },
     {
-      colorName: 'purple',
-      bgColor: 'bg-[#a855f7]',
-      hex: 'a855f7',
+      colorName: "purple",
+      bgColor: "bg-[#a855f7]",
+      hex: "a855f7",
     },
     {
-      colorName: 'pink',
-      bgColor: 'bg-[#ec4899]',
-      hex: 'ec4899',
+      colorName: "pink",
+      bgColor: "bg-[#ec4899]",
+      hex: "ec4899",
     },
     {
-      colorName: 'default-grey',
-      bgColor: 'bg-[#444e59]',
-      hex: '444e59',
+      colorName: "default-grey",
+      bgColor: "bg-[#444e59]",
+      hex: "444e59",
     },
     {
-      colorName: 'slate',
-      bgColor: 'bg-[#64748b]',
-      hex: '64748b',
+      colorName: "slate",
+      bgColor: "bg-[#64748b]",
+      hex: "64748b",
     },
   ],
   darkColors: [
     {
-      colorName: 'white',
-      bgColor: 'bg-[#ffffff]',
-      hex: 'ffffff',
+      colorName: "white",
+      bgColor: "bg-[#ffffff]",
+      hex: "ffffff",
     },
     {
-      colorName: 'black',
-      bgColor: 'bg-[#000000]',
-      hex: '000000',
+      colorName: "black",
+      bgColor: "bg-[#000000]",
+      hex: "000000",
     },
     {
-      colorName: 'red-dark',
-      bgColor: 'bg-[#7f1d1d]',
-      hex: '7f1d1d',
+      colorName: "red-dark",
+      bgColor: "bg-[#7f1d1d]",
+      hex: "7f1d1d",
     },
     {
-      colorName: 'orange-dark',
-      bgColor: 'bg-[#7c2d12]',
-      hex: '7c2d12',
+      colorName: "orange-dark",
+      bgColor: "bg-[#7c2d12]",
+      hex: "7c2d12",
     },
     {
-      colorName: 'yellow-dark',
-      bgColor: 'bg-[#713f12]',
-      hex: '713f12',
+      colorName: "yellow-dark",
+      bgColor: "bg-[#713f12]",
+      hex: "713f12",
     },
     {
-      colorName: 'lime-dark',
-      bgColor: 'bg-[#365314]',
-      hex: '365314',
+      colorName: "lime-dark",
+      bgColor: "bg-[#365314]",
+      hex: "365314",
     },
     {
-      colorName: 'green-dark',
-      bgColor: 'bg-[#14532d]',
-      hex: '14532d',
+      colorName: "green-dark",
+      bgColor: "bg-[#14532d]",
+      hex: "14532d",
     },
     {
-      colorName: 'emerald-dark',
-      bgColor: 'bg-[#134e4a]',
-      hex: '134e4a',
+      colorName: "emerald-dark",
+      bgColor: "bg-[#134e4a]",
+      hex: "134e4a",
     },
     {
-      colorName: 'blue-dark',
-      bgColor: 'bg-[#1e3a8a]',
-      hex: '1e3a8a',
+      colorName: "blue-dark",
+      bgColor: "bg-[#1e3a8a]",
+      hex: "1e3a8a",
     },
     {
-      colorName: 'indigo-dark',
-      bgColor: 'bg-[#312e81]',
-      hex: '312e81',
+      colorName: "indigo-dark",
+      bgColor: "bg-[#312e81]",
+      hex: "312e81",
     },
     {
-      colorName: 'purple-dark',
-      bgColor: 'bg-[#581c87]',
-      hex: '581c87',
+      colorName: "purple-dark",
+      bgColor: "bg-[#581c87]",
+      hex: "581c87",
     },
     {
-      colorName: 'pink-dark',
-      bgColor: 'bg-[#831843]',
-      hex: '831843',
+      colorName: "pink-dark",
+      bgColor: "bg-[#831843]",
+      hex: "831843",
     },
     {
-      colorName: 'stone-dark',
-      bgColor: 'bg-[#1c1917]',
-      hex: '1c1917',
+      colorName: "stone-dark",
+      bgColor: "bg-[#1c1917]",
+      hex: "1c1917",
     },
     {
-      colorName: 'gray-dark',
-      bgColor: 'bg-[#171717]',
-      hex: '171717',
+      colorName: "gray-dark",
+      bgColor: "bg-[#171717]",
+      hex: "171717",
     },
     {
-      colorName: 'dark-900',
-      bgColor: 'bg-[#181824]',
-      hex: '181824',
+      colorName: "dark-900",
+      bgColor: "bg-[#181824]",
+      hex: "181824",
     },
     {
-      colorName: 'slate-dark',
-      bgColor: 'bg-[#0f172a]',
-      hex: '0f172a',
+      colorName: "slate-dark",
+      bgColor: "bg-[#0f172a]",
+      hex: "0f172a",
     },
     {
-      colorName: '[#22272e]',
-      bgColor: 'bg-[#22272e]',
-      hex: '22272e',
+      colorName: "[#22272e]",
+      bgColor: "bg-[#22272e]",
+      hex: "22272e",
     },
     {
-      colorName: 'zinc-dark',
-      bgColor: 'bg-[#27272a]',
-      hex: '27272a',
+      colorName: "zinc-dark",
+      bgColor: "bg-[#27272a]",
+      hex: "27272a",
     },
   ],
 };
@@ -1031,8 +1030,6 @@ export const supportStore = {
 
 // Color State
 const initialState = {
-  section: 'introduction',
-  reconst initialState = {
   section: "introduction",
   renderMode: "preview",
   // Introduction State
@@ -1065,7 +1062,8 @@ const initialState = {
       linkSuffix: "",
     },
     gitlab: {
-      path: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/gitlab.svg",
+      label: "GitLab",
+      path: `${PROJECT_URL}/icons/socials/gitlab.svg`,
       linkPrefix: "https://www.gitlab.com/",
       linkSuffix: "",
     },
@@ -1449,9 +1447,9 @@ function MyApp({ Component, pageProps }) {
     const handleRouteChange = (url) => {
       gtag.pageview(url);
     };
-    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
 
@@ -1473,13 +1471,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        strategy='afterInteractive'
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        id='analytics-tag'
+        id="analytics-tag"
       />
       <Script
-        strategy='afterInteractive'
-        id='analytics-config'
+        strategy="afterInteractive"
+        id="analytics-config"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
@@ -1491,7 +1489,7 @@ function MyApp({ Component, pageProps }) {
           `,
         }}
       />
-      <ThemeProvider enableSystem={true} attribute='class'>
+      <ThemeProvider enableSystem={true} attribute="class">
         <StateContext.Provider value={{ state, dispatch }}>
           {getLayout(<Component {...pageProps} />)}
         </StateContext.Provider>
